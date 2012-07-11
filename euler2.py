2b4b3d91dde11a050badb80a1ea9df4ef2892675
Alby Reid <alby@bleary-id.co.uk>
2012-07-11 23:43:13 +0100

    Problem 2 in progress

---
+++ euler2.py
@@ -0,0 +1,10 @@
+# Check I'm able to generate the Fibonacci sequence up to 4000000
+# Define variables
+a = 0
+b = 1
+total = 1
+while total <= 4000000:
+    print (total)
+    total = a + b
+    a = b
+    b = total
