b0d77dfabbb954d352e657c955d92473322014bb
Alby Reid <alby@bleary-id.co.uk>
2012-07-12 00:02:04 +0100

    Solved Problem 2

--- euler2.py
+++ euler2.py
@@ -3,8 +3,15 @@
 a = 1
 b = 1
 total = 1
+answer = 0
+# Begin loop
 while total <= 4000000:
     print (total)
     total = a + b
     a = b
     b = total
+    # Check if current term is even
+    if total % 2 == 0:
+        # If current term is even, add to total
+        answer = answer + total
+print (answer)
