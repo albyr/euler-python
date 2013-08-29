4eb29dea767cb2d9ce1440061b5cf5aa0b5df525
Alby Reid <alby@bleary-id.co.uk>
2013-08-30 00:01:21 +0100

    Better solution
    Halved runtime to about 500 ms.

--- euler4.py
+++ euler4.py
@@ -5,8 +5,12 @@
 # Initialise variables
 palindromes = []
 
+# Import date and time function for logging
+import datetime
+
 # Function that checks if a number is palindromic
 def isPalindrome(n):
+    # Uses extended slice syntax, without start or end and with step -1, to reverse string representation of n
     if str(n) == str(n)[::-1]:
         palindromic = True
     else:
@@ -14,8 +18,10 @@ def isPalindrome(n):
     return palindromic
 
 # This is an incredibly slow way of doing this. Needs fixing.
-# Loop through x and y from 100 to 999
+starttime = datetime.datetime.now()
+# Generate all possible products of two 3-digit numbers
 for x in range(100,1000):
+    # Only need to check from x to 1000 because otherwise we repeat calculations
     for y in range(100,1000):
         z = x * y
         # Is z a palindrome?
@@ -26,3 +32,4 @@ for x in range(100,1000):
 
 # Find the largest palindrome and print it out
 print ("The largest palindrome is " + str(max(palindromes)))
+print (datetime.datetime.now()-starttime)
