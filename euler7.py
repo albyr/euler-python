ad4847a11e1791d6bad17d5e43da7de234a0595a
Alby Reid <alby@bleary-id.co.uk>
2013-08-30 14:00:56 +0100

    Replaced sqrt() with **0.5
    Now I don't have to import sqrt from math.

--- euler7.py
+++ euler7.py
@@ -12,7 +12,7 @@ primeCount = 0
 # Function (re-used from problem 3) to check whether a number is prime
 def checkprime(n):
     # Trial division
-    for i in range(2,int(sqrt(n)+2)):
+    for i in range(2,int(n**0.5)+2):
         if n < 2:
             isPrime = False
         elif n == 2:
