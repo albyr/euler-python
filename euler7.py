2a1cf57bea41d168ebf27a14f1bc0451be9fbb62
Alby Reid <alby@bleary-id.co.uk>
2013-08-31 19:40:31 +0100

    More efficient solution to problem 7
    Uses a simplified isPrime function.

--- euler7.py
+++ euler7.py
@@ -9,27 +9,26 @@ from math import sqrt
 checkIsPrime = 1
 primeCount = 0
 
-# Function (re-used from problem 3) to check whether a number is prime
-def checkprime(n):
-    # Trial division
-    for i in range(2,int(n**0.5)+2):
-        if n < 2:
-            isPrime = False
-        elif n == 2:
-            isPrime = True
-        elif n % i == 0:
-            # n mod i is equal to zero and therefore n is not prime
-            isPrime = False
-            # Exit loop to prevent isPrime from subsequently being set true
-            break
-        elif n % i != 0:
-            # n might be prime, so set isPrime equal to true
-            isPrime = True
-    return isPrime
+# Function (better than that used in problem 3) to check whether a number is prime
+def checkPrime(n):
+    if n < 2:
+        return False # 0 and 1 are not prime numbers
+    if n == 2:
+        return True # 2 is the first prime number
+    elif n % 2 == 0:
+        return False # No even numbers (other than 2) are prime numbers
+    for i in range(3,int(n**0.5)+1,2): # Only checks odd numbers, starting at 3
+        if n % i == 0:
+            # n mod i is equal to zero and therefore n cannot be prime
+            return False
+    # n is not 0, 1, 2 and does not have any factors other than 1 or itself and is therefore prime
+    return True
 
 while primeCount != 10001:
-    if checkprime(checkIsPrime) == True:
+    print ("Checking " + str(checkIsPrime))
+    if checkPrime(checkIsPrime) == True:
         primeCount += 1
+        print (str(checkIsPrime) + " is prime number " + str(primeCount))
     if primeCount == 10001:
         break
     else:
