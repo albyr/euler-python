f08cb71c8545d8968356300cd89e8f123a91f59a
Alby Reid <alby@bleary-id.co.uk>
2013-09-17 16:14:00 +0100

    Added possibly useful website

--- euler9.py
+++ euler9.py
@@ -4,3 +4,5 @@
 # For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
+
+# This might be useful: https://en.wikipedia.org/wiki/Formulas_for_generating_Pythagorean_triples
